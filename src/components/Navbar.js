cac61f2fed457d47a6a0b0256c90728c77b23c75
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-04-06 19:25:49 -0400

    active link underline

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,19 @@ import { Link } from "gatsby"
 const Navbar = () => {
   return (
     <header className="container flex flex-col md:flex-row justify-between items-center h-20 mt-5 mb-12">
-      <Link to="/" className="text-2xl font-medium tracking-wider">ANDY MILLER III</Link>
+      <Link to="/" className="text-2xl font-medium tracking-wider">
+        ANDY MILLER III
+      </Link>
       <nav className="md:text-lg space-x-5 tracking-wide">
-        <Link to="/">HOME</Link>
-        <Link to="/articles/">ARTICLES</Link>
-        <Link>MEDIA</Link>
-        <Link>ABOUT</Link>
-        <Link>CONTACT</Link>
+        <Link to="/" activeClassName="underline">
+          HOME
+        </Link>
+        <Link to="/articles/" activeClassName="underline">
+          ARTICLES
+        </Link>
+        <Link activeClassName="underline">MEDIA</Link>
+        <Link activeClassName="underline">ABOUT</Link>
+        <Link activeClassName="underline">CONTACT</Link>
       </nav>
     </header>
   )
