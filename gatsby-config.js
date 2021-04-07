492ac5b62d04999b07879554d1a500cd3792d070
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-04-07 17:08:57 -0400

    add gatsby cloud plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
       },
     },
     "gatsby-plugin-react-helmet",
+    `gatsby-plugin-gatsby-cloud`,
     "gatsby-plugin-image",
     {
       resolve: `gatsby-source-filesystem`,
