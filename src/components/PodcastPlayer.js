542bcc6cbc9915e4847b172381ab9e382def8aaf
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-04-08 19:29:34 -0400

    fix podcast player loading error

--- src/components/PodcastPlayer.js
+++ src/components/PodcastPlayer.js
@@ -1,24 +1,25 @@
 import React from "react"
-// import { Helmet } from "react-helmet"
+import { Helmet } from "react-helmet"
 
 const PodcastPlayer = () => {
   return (
     <div>
-      {/* <Helmet>
+      <Helmet>
           <script
             type="text/javascript"
             charset="utf-8"
+            defer
             src="https://www.buzzsprout.com/362981.js?player=small&limit=1&container_id=buzzsprout"
           ></script>
         </Helmet>
-        <div id="buzzsprout"></div> */}
-      <iframe
+        <div id="buzzsprout"></div>
+      {/* <iframe
         className="w-full overflow-hidden"
         id="buzzsprout"
         scrolling="no"
         title="Podcast Player"
         src="https://www.buzzsprout.com/362981/8283813-easter-sunday-message?client_source=small_player&amp;iframe=true"
-      ></iframe>
+      ></iframe> */}
     </div>
   )
 }
