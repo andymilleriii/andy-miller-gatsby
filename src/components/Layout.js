27ac5fce547facfdb34eb3bb9235178c38708f5e
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-04-09 12:26:39 -0400

    change footer spacing

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
       <div className="container min-h-screen">
         <div className="">{children}</div>
       </div>
-      <div className=" bg-gray-800 text-white mt-40">
+      <div className=" bg-gray-800 text-white mt-32">
         <div className="container flex flex-col md:flex-row justify-center md:justify-between h-24 items-center">
           <p>Copyright © 2021 Andrew S. Miller III</p>
           <p>Contact information</p>
