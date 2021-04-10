338da8b004c75ba469d55d15ad3a5f89fec48054
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-04-10 08:33:31 -0400

    podcast player spacing

--- src/pages/media/podcast.js
+++ src/pages/media/podcast.js
@@ -1,8 +1,26 @@
 import React from "react"
 import Layout from "../../components/Layout"
 import { Helmet } from "react-helmet"
+import { graphql } from "gatsby"
+import SanityImage from "gatsby-plugin-sanity-image";
 
-const PodcastPage = () => {
+export const query = graphql`
+  {
+    sanityPodcast {
+      title
+      description
+      links {
+        _key
+        image {
+          ...Image
+        }
+        url
+      }
+    }
+  }
+`
+
+const PodcastPage = ({data:{sanityPodcast:{title, description, links}}}) => {
   return (
     <Layout>
       <Helmet>
@@ -14,8 +32,22 @@ const PodcastPage = () => {
           src="https://www.buzzsprout.com/362981.js?container_id=buzzsprout-large-player&player=large"
         ></script>
       </Helmet>
-      <h1 className="text-4xl uppercase">Podcast</h1>
+      <h1 className="text-4xl uppercase">{title}</h1>
       <hr className="my-14 border-red-900 border-solid"></hr>
+      <p className="text-xl my-14">{description}</p>
+      <div className="grid grid-cols-2 gap-5 md:flex md:space-x-5 mb-14">
+        {links.map((link) => (
+          <a
+            href={link.url}
+            target="_blank"
+            rel="noreferrer"
+            key={link._key}
+            width="200"
+          >
+            <SanityImage {...link.image}></SanityImage>
+          </a>
+        ))}
+      </div>
       <div id="buzzsprout-large-player"></div>
     </Layout>
   )
