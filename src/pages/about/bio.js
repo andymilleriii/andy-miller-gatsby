c78491bcb90aedb9b7945447437911802dea300b
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-04-10 13:37:04 -0400

    format bio page

--- src/pages/about/bio.js
+++ src/pages/about/bio.js
@@ -21,9 +21,14 @@ const BioPage = ({data:{sanityAbout:{_rawBio, image, name}}}) => {
     <Layout>
       <h1 className="text-4xl uppercase">Bio</h1>
       <hr className="my-14 border-red-900 border-solid"></hr>
-      <div>
-        <SanityImage {...image} alt={name}></SanityImage>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-14">
         <FormattedPortableText content={_rawBio}></FormattedPortableText>
+        <SanityImage
+          {...image}
+          alt={name}
+          width="400"
+          className="max-h-96 w-full object-cover rounded-md"
+        ></SanityImage>
       </div>
     </Layout>
   )
