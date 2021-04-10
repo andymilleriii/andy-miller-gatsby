8301f8682c2eed68aeba96dfe39fea8f6f13cb8b
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-04-10 14:50:48 -0400

    book page designed

--- src/pages/media/books.js
+++ src/pages/media/books.js
@@ -1,25 +1,57 @@
 import React from "react"
 import Layout from "../../components/Layout"
+import SanityImage from "gatsby-plugin-sanity-image"
 import { graphql } from "gatsby"
 
 export const query = graphql`
   {
-    sanityVideoPage {
-      videoLinks {
+    sanityBookPage {
+      books {
+        image {
+          ...ImageWithPreview
+        }
+        description
+        link
+        subtitle
         title
-        url
-        _key
       }
     }
   }
 `
 
-const BookPage = () => {
+const BookPage = ({
+  data: {
+    sanityBookPage: { books },
+  },
+}) => {
   return (
     <Layout>
       <h1 className="text-4xl uppercase">Books</h1>
       <hr className="my-14 border-red-900 border-solid"></hr>
-      <div className="grid grid-cols-3"></div>
+      <div className="space-y-20">
+      {books.map((book) => (
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-14 mb-10">
+          <SanityImage
+            {...book.image}
+            alt={book.title}
+            width="500"
+            className="w-full"
+          ></SanityImage>
+          <div className="col-span-1 xl:col-span-2">
+            <h1 className="text-4xl mb-10">{book.title}</h1>
+            <p className="text-lg mb-10">{book.description}</p>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={book.link}
+              className="text-red-800 border  bg-white border-red-800 text-center rounded-md py-2 px-4 uppercase hover:bg-red-50"
+            >
+              Purchase
+            </a>
+          </div>
+        </div>
+        ))}
+        </div>
     </Layout>
   )
 }
