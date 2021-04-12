377e2cc65068eda789636153f0e9791c37100527
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-04-12 13:04:31 -0400

    design 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,23 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import { Link } from "gatsby";
+import React from 'react'
+import Layout from "../components/Layout";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
+    <Layout>
+      <h1 className="text-5xl text-center mt-40">Page Not Found</h1>
+      <p className="text-xl text-center my-10">Sorry, the page you are looking for cannot be found.</p>
+      <p className="text-center">
+        <Link
+          to="/"
+          className="text-red-800 border  bg-white border-red-800 text-center rounded-md py-2 px-4 uppercase hover:bg-red-50"
+        >
+          Return Home
+        </Link>
       </p>
-    </main>
+    </Layout>
   )
 }
 
 export default NotFoundPage
+
