a7c7b6fa6ad2deeb74600fd79b4ba124ba4d3f5c
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-04-17 13:22:32 -0400

    use formspree react package

--- src/pages/about/speaking-request.js
+++ src/pages/about/speaking-request.js
@@ -1,74 +1,76 @@
-import React, {useEffect} from "react"
+import React from "react"
 import Layout from "../../components/Layout"
+import { useForm } from "@formspree/react"
 
 const SpeakingRequestPage = () => {
-  useEffect(() => {
-    clear()
-  })
-  const clear = () => {
-    document.getElementById("form").reset()
-  }
+  const [state, handleSubmit] = useForm("xnqlqwdq")
   return (
     <Layout>
       <h1 className="text-4xl uppercase">Speaking Requests</h1>
       <hr className="my-14 border-red-900 border-solid"></hr>
-      <form
-        method="post"
-        action="https://formspree.io/f/xnqlqwdq"
-        className="block max-w-xl space-y-5"
-        id="form"
-      >
-        <label className="block">
-          <span className="text-gray-700">Full Name</span>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            required
-            className="mt-1 block w-full rounded"
-          />
-        </label>
-        <label className="block">
-          <span className="text-gray-700">Email</span>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            required
-            className="mt-1 block w-full rounded"
-          />
-        </label>
-        <label className="block">
-          <span className="text-gray-700">Name of Event</span>
-          <input
-            type="text"
-            name="subject"
-            id="subject"
-            className="mt-1 block w-full rounded"
-          />
-        </label>
-        <label class="block">
-          <span class="text-gray-700">When is your event?</span>
-          <input type="date" name="event_date" class="mt-1 block w-full rounded" id="date"/>
-        </label>
-        <label className="block">
-          <span className="text-gray-700">Please provide details for your request:</span>
-          <textarea
-            name="message"
-            id="message"
-            rows="5"
-            required
-            className="mt-1 block w-full rounded"
-          />
-        </label>
-        <input type="text" name="_gotcha" className="hidden" />
-        <button
-          type="submit"
-          className="text-lg rounded border border-black bg-gray-100 py-1 px-4"
-        >
-          Send
-        </button>
-      </form>
+      {state.succeeded ? (
+        <p>Thank you, your submission has been received.</p>
+      ) : (
+        <form className="block max-w-xl space-y-5" onSubmit={handleSubmit}>
+          <label className="block">
+            <span className="text-gray-700">Full Name</span>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              required
+              className="mt-1 block w-full rounded"
+            />
+          </label>
+          <label className="block">
+            <span className="text-gray-700">Email</span>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              required
+              className="mt-1 block w-full rounded"
+            />
+          </label>
+          <label className="block">
+            <span className="text-gray-700">Name of Event</span>
+            <input
+              type="text"
+              name="subject"
+              id="subject"
+              className="mt-1 block w-full rounded"
+            />
+          </label>
+          <label class="block">
+            <span class="text-gray-700">When is your event?</span>
+            <input
+              type="date"
+              name="event_date"
+              class="mt-1 block w-full rounded"
+              id="date"
+            />
+          </label>
+          <label className="block">
+            <span className="text-gray-700">
+              Please provide details for your request:
+            </span>
+            <textarea
+              name="message"
+              id="message"
+              rows="5"
+              required
+              className="mt-1 block w-full rounded"
+            />
+          </label>
+          <input type="text" name="_gotcha" className="hidden" />
+          <button
+            type="submit"
+            className="text-lg rounded border border-black bg-gray-100 py-1 px-4"
+          >
+            Send
+          </button>
+        </form>
+      )}
     </Layout>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,71 +1,68 @@
-import React, { useEffect } from "react"
+import React from "react"
 import Layout from "../components/Layout"
+import { useForm } from "@formspree/react"
 
 const ContactPage = () => {
-  useEffect(() => {
-    clear()
-  })
-  const clear = () => {
-    document.getElementById("form").reset()
-  }
+  const [state, handleSubmit] = useForm("xrgrgjpn")
   return (
     <Layout>
       <h1 className="text-4xl uppercase">Contact</h1>
       <hr className="my-14 border-red-900 border-solid"></hr>
-      <form
-        id="form"
-        method="post"
-        action="https://formspree.io/f/xrgrgjpn"
-        className="block max-w-xl space-y-5"
-      >
-        <label className="block">
-          <span className="text-gray-700">Full Name</span>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            required
-            className="mt-1 block w-full rounded"
-          />
-        </label>
-        <label className="block">
-          <span className="text-gray-700">Email</span>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            required
-            className="mt-1 block w-full rounded"
-          />
-        </label>
-        <label className="block">
-          <span className="text-gray-700">Subject</span>
-          <input
-            type="text"
-            name="subject"
-            id="subject"
-            required
-            className="mt-1 block w-full rounded"
-          />
-        </label>
-        <label className="block">
-          <span className="text-gray-700">Message</span>
-          <textarea
-            name="message"
-            id="message"
-            rows="5"
-            required
-            className="mt-1 block w-full rounded"
-          />
-        </label>
-        <input type="text" name="_gotcha" className="hidden" />
-        <button
-          type="submit"
-          className="text-lg rounded border border-black bg-gray-100 py-1 px-4"
-        >
-          Send
-        </button>
-      </form>
+      {state.succeeded ? (
+        <div>
+          <p>Thank you, your submission has been received.</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit} className="block max-w-xl space-y-5">
+          <label className="block">
+            <span className="text-gray-700">Full Name</span>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              required
+              className="mt-1 block w-full rounded"
+            />
+          </label>
+          <label className="block">
+            <span className="text-gray-700">Email</span>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              required
+              className="mt-1 block w-full rounded"
+            />
+          </label>
+          <label className="block">
+            <span className="text-gray-700">Subject</span>
+            <input
+              type="text"
+              name="subject"
+              id="subject"
+              required
+              className="mt-1 block w-full rounded"
+            />
+          </label>
+          <label className="block">
+            <span className="text-gray-700">Message</span>
+            <textarea
+              name="message"
+              id="message"
+              rows="5"
+              required
+              className="mt-1 block w-full rounded"
+            />
+          </label>
+          <input type="text" name="_gotcha" className="hidden" />
+          <button
+            type="submit"
+            className="text-lg rounded border border-black bg-gray-100 py-1 px-4"
+          >
+            Send
+          </button>
+        </form>
+      )}
     </Layout>
   )
 }
