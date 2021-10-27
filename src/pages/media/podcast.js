7db26126de0a6488d78bdbb5d1a6361463848ac7
Brenden Moore <bmoore@accusoft.com>
2021-10-27 10:44:12 -0400

    update captain's corner archive link

--- src/pages/media/podcast.js
+++ src/pages/media/podcast.js
@@ -25,12 +25,6 @@ const PodcastPage = ({data:{sanityPodcast:{title, description, links}}}) => {
     <Layout>
       <Helmet>
         <title>Andy Miller III - Podcast</title>
-        <script
-          defer
-          type="text/javascript"
-          charset="utf-8"
-          src="https://www.buzzsprout.com/362981.js?container_id=buzzsprout-large-player&tags=Captian's%20Corner,%20Captain's%20Corner&player=large"
-        ></script>
       </Helmet>
       <h1 className="text-4xl uppercase">{title}</h1>
       <hr className="my-14 border-red-900 border-solid"></hr>
@@ -52,7 +46,7 @@ const PodcastPage = ({data:{sanityPodcast:{title, description, links}}}) => {
       <h1 className="text-4xl mt-14 uppercase">Captain's Corner</h1>
       <hr className="my-14 border-red-900 border-solid"></hr>
       <p className="text-xl my-14">Listen to an Archive of Captain's Corner</p>
-      <div id="buzzsprout-large-player"></div>
+      <iframe title="podcast" width="100%" height="390" frameborder="no" scrolling="no" seamless src="https://share.transistor.fm/e/captains-corner-podcast-archives/playlist"></iframe>
     </Layout>
   )
 }
