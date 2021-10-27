d23b9f6befbdd82f9aa4314584469c3e45bbdeb6
Brenden Moore <bmoore@accusoft.com>
2021-10-27 14:13:06 -0400

    style links

--- src/pages/media/podcast.js
+++ src/pages/media/podcast.js
@@ -28,7 +28,7 @@ const PodcastPage = ({data:{sanityPodcast:{title, description, links}}}) => {
       </Helmet>
       <h1 className="text-4xl uppercase">{title}</h1>
       <hr className="my-14 border-red-900 border-solid"></hr>
-      <p className="text-xl my-14">{description} <a href="https://feeds.transistor.fm/more-to-the-story-with-dr-andy-miller">View All Episodes</a></p>
+      <p className="text-xl my-14">{description} <a class="text-red-800 underline" href="https://feeds.transistor.fm/more-to-the-story-with-dr-andy-miller">View All Episodes</a></p>
       <div className="grid grid-cols-2 gap-5 md:flex md:space-x-5 mb-14">
         {links.map((link) => (
           <a
@@ -45,7 +45,7 @@ const PodcastPage = ({data:{sanityPodcast:{title, description, links}}}) => {
       <iframe title="podcast" width="100%" height="390" frameborder="no" scrolling="no" seamless src="https://share.transistor.fm/e/more-to-the-story-with-dr-andy-miller/playlist"></iframe>
       <h1 className="text-4xl mt-14 uppercase">Captain's Corner</h1>
       <hr className="my-14 border-red-900 border-solid"></hr>
-      <p className="text-xl my-14">Listen to an Archive of Captain's Corner <a href="https://feeds.transistor.fm/captains-corner-podcast-archives">View All Episodes</a></p>
+      <p className="text-xl my-14">Listen to an Archive of Captain's Corner. <a class="text-red-800 underline" href="https://feeds.transistor.fm/captains-corner-podcast-archives">View All Episodes</a></p>
       <iframe title="podcast" width="100%" height="390" frameborder="no" scrolling="no" seamless src="https://share.transistor.fm/e/captains-corner-podcast-archives/playlist"></iframe>
     </Layout>
   )
